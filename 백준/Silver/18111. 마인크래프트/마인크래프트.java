8089098f6811b1d3b71846616dc9c9e2de64a0bf
DKS2312 <59419591+dks2312@users.noreply.github.com>
2022-06-13 16:53:32 +0900

    [Silver II] Title: 마인크래프트, Time: 612 ms, Memory: 37172 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -3,58 +3,67 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//1. 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다.
-//2. 인벤토리에서 블록 하나를 꺼내어 좌표 (i, j)의 가장 위에 있는 블록 위에 놓는다.
-
-public class Main {
-	public static void main(String[] args) throws IOException {
+class Main{
+    public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
 		
-		int[][] map = new int[N][M];
-		int max = 0, min = Integer.MAX_VALUE;
+        int N = Integer.parseInt(st.nextToken()); //세로
+        int M = Integer.parseInt(st.nextToken()); //가로
+        int B = Integer.parseInt(st.nextToken()); //인벤토리 블록 겟수
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+        int[][] world = new int[N][M];
 
-				if (max < map[i][j]) max = map[i][j];
-				else if (min > map[i][j]) min = map[i][j];
-			}
-		}
-		
-		int answerSeconds = Integer.MAX_VALUE;
-		int answerHeight = -1; 
-	    
-		for (int heigh = min; heigh <= max; heigh++) {
-			int second = 0;
-			int inventory = B;
+        int max_world = 0;
+        int min_world = 256;
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					int diff = map[i][j] - heigh;
-					
-					if(diff > 0) {
-						second += diff*2;
-						inventory += diff;
-					}else if(diff < 0){
-						second += Math.abs(diff);
-						inventory -= Math.abs(diff);
-					}
-				}
-			}
-			
-			if(inventory >= 0 && second <= answerSeconds) {
-				answerSeconds = second;
-				answerHeight = heigh;
-			}
-		}
-		
-		System.out.println(answerSeconds +" "+ answerHeight);
-	}
+        for(int i = 0; i < N; i++){
+        	st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                world[i][j] = Integer.parseInt(st.nextToken());
+                
+                if(world[i][j] > max_world) max_world = world[i][j];
+                if(world[i][j] < min_world) min_world = world[i][j];
+            }
+        }
+
+        // System.out.println("max = " + max_world);
+        // System.out.println("min = " + min_world);
+
+        //제일 낮은곳을 채우면서 해야하는 시간량과
+        //제일 높은곳을 깍으면서 해야하는 시간량을 계산한다.
+        //낮은곳부터 채우다가 인벤토리블럭이 없으면 탈출한다.
+        int time = 125000000, lev = -1;
+        all: for(int w = max_world; w >= min_world; w--){
+            int inputblock = 0; //채우는 블럭
+            int breakblock = 0; //부수는 블럭
+            int blockstock = B;
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    //기준보다 높을시 블럭을 깍는다.
+                    if(world[i][j] > w){
+                        //깍아야 하는 블럭갯수
+                        breakblock += world[i][j] - w;
+                        //인밴토리에 추가되는 블럭 갯수
+                        blockstock += world[i][j] - w;
+                    }
+                    //기준보다 작을시 블럭을 채운다.
+                    if(world[i][j] < w){
+                        //채워야하는 블럭갯수
+                        inputblock += w - world[i][j];
+                        //인벤토리에 감소되는 블럭갯수
+                        blockstock -= w - world[i][j];
+                    }
+                }
+            }
+            //사용해야하는 블럭이 모자라면 다음 케이스
+            if(blockstock < 0) continue all;
+            //블럭이 최대한 딱맞아 떯어지면 그게 최적값이다.
+            if(inputblock + (breakblock * 2) >= time) break all;
+            
+            time = inputblock + (breakblock * 2);
+            lev = w;
+        }
+        System.out.println(time + " " + lev);
+    }
 }
