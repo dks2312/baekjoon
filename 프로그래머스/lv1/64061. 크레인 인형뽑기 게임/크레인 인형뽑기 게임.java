fd1ae76e832373413bba468a248507d61d65fb8b
DKS2312 <59419591+dks2312@users.noreply.github.com>
2022-06-21 13:58:38 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.22 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,39 +1,36 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        
-        ArrayList<Queue<Integer>> QList = new ArrayList<Queue<Integer>>(board.length);
-        Stack<Integer> stack = new Stack<Integer>();
-        
-        for(int i = 0; i < board[0].length; i++) {
-        	Queue<Integer> QTmp = new LinkedList<Integer>();
-        	
-        	for(int j = 0; j < board.length; j++) {
-        		if(board[j][i] != 0)
-        			QTmp.offer(board[j][i]);
-        	}
-        	
-        	QList.add(QTmp);
+
+        ArrayList<Stack<Integer>> board_stack = new ArrayList<Stack<Integer>>();
+        Stack<Integer> basket = new Stack<Integer>();
+
+        for(int j = 0; j < board[0].length; j++){
+            board_stack.add(new Stack<Integer>());
+
+            for(int i = board.length-1; i >= 0; i--){
+                if(board[i][j] != 0)
+                    board_stack.get(j).push(board[i][j]);
+            }
         }
-        
-        for(int move : moves) {
-        	if(QList.get(move-1).isEmpty()) 
-        		continue;
-        	
-        	int e = QList.get(move-1).poll();
-        	
-    		if(!stack.isEmpty() && stack.peek() == e) {
-    			stack.pop();
-    			answer += 2;
-    		}
-    		else stack.push(e);
+
+        for(int move : moves){
+            if(board_stack.get(move-1).isEmpty()) 
+                continue;
+
+            int tmp = board_stack.get(move-1).pop();
+
+            if(!basket.empty() && basket.peek() == tmp){
+                basket.pop();
+                answer += 2;
+            }
+            else {
+                basket.push(tmp);
+            }
         }
-        
+
         return answer;
     }
 }
